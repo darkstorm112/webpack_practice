75daf0751048f75b273f7e20a236dba0136310d8
darkstorm112 <1007793642@qq.com>
2021-05-09 00:28:16 +0800

    css配置完成

--- 02webpack配置文件（css）/wk.config.js
+++ 02webpack配置文件（css）/wk.config.js
@@ -13,12 +13,32 @@ module.exports = {
         rules:[
             {
                 test:/\.css$/,
-                use:["style-loader", "css-loader"]
+                use:[
+                        "style-loader", 
+                        {
+                            loader: "css-loader",
+                            options: {
+                                importLoaders: 1
+                            }
+                        }, 
+                        "postcss-loader"
+                    ]
             },
             {
                 test:/\.less$/,
-                use:["style-loader", "css-loader", "less-loader"]
-            }
+                use:[
+                    "style-loader", 
+                    {
+                        loader: "css-loader",
+                        options: {
+                            importLoaders: 2
+                        }
+                    },
+                    "postcss-loader", 
+                    "less-loader"
+                ]
+            },
+            
         ]
     }
 }
